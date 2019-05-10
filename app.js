00716c0a8df88270231226157a33e4a729b5e571
Asa Smith <smith.asa.la@gmail.com>
2019-05-09 23:57:24 -0400

    add routes for /products and /products/:id

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const axios = require('axios')
+
 const app = express()
 
 app.get('/', (req, res) => {
   return res.send('hello world from server.js')
+})
+
+// get all products
+app.get('/products', async (req, res) => {
+  const { data } =  await axios.get('https://next.json-generator.com/api/json/get/EkzBIUWNL')
+  res.json(data)
+})
+
+// get individual product using URL param
+app.get('/products/:id', async (req, res) => {
+  const id = req.params.id
+  const { data } =  await axios.get('https://next.json-generator.com/api/json/get/EkzBIUWNL')
+
+  console.log(typeof data)
+  const item = data.find(item => item._id === id)
+  res.json(item)
 })
 
 module.exports = app
